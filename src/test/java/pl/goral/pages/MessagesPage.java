347ccbf58979705bab31ac07c79c75cb692033ca
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-04-02 20:33:53 +0200

    first commit

---
+++ src/test/java/pl/goral/pages/MessagesPage.java
@@ -0,0 +1,21 @@
+package pl.goral.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class MessagesPage extends BasePage {
+
+    @FindBy(id = "j_msgContent")
+    private WebElement messageTextArea;
+
+    protected MessagesPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void waitForTextAreaToLoad() {
+        wait.until(ExpectedConditions.elementToBeClickable(messageTextArea));
+    }
+
+}
