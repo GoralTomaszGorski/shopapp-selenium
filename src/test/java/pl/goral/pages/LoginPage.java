aef127367ee64638da8c8189b3755671888ee4cd
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-04-03 22:21:51 +0200

    Login improve

--- src/test/java/pl/goral/pages/LoginPage.java
+++ src/test/java/pl/goral/pages/LoginPage.java
@@ -1,26 +1,30 @@
 package pl.goral.pages;
 
-import lombok.SneakyThrows;
-import org.openqa.selenium.By;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import pl.goral.components.Toast;
 import pl.goral.config.ConfigProvider;
 
 public class LoginPage extends BasePage {
 
     @FindBy(id = "username")
-    private WebElement emailField;
+    private WebElement usernameInput;
 
     @FindBy(id = "password")
-    private WebElement passwordField;
+    private WebElement passwordInput;
 
     @FindBy(xpath = "//button[@type='submit' and contains(text(), 'Sign in')]")
-    private WebElement loginButton;
+    private WebElement signInButton;
+
+    @Getter
+    private final Toast toast;
+
 
     public LoginPage(WebDriver driver) {
         super(driver);
+        toast = new Toast(driver);
     }
 
     public LoginPage openPage() {
@@ -28,11 +32,10 @@ public class LoginPage extends BasePage {
         return this;
     }
 
-    @SneakyThrows
-    public HomePage attemptLogin(String email, String password) {
-        emailField.sendKeys(email);
-        passwordField.sendKeys(password);
-        loginButton.click();
-        return new HomePage(driver);
+    public <T extends BasePage> T attemptLogin(String username, String password, Class<T> expectedPage) {
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        signInButton.click();
+        return getInstance(expectedPage);
     }
 }
--- src/test/java/pl/goral/tests/LoginTest.java
+++ src/test/java/pl/goral/tests/LoginTest.java
@@ -3,6 +3,7 @@ package pl.goral.tests;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import pl.goral.config.ConfigProvider;
+import pl.goral.pages.LoggedInHomePage;
 import pl.goral.pages.LoginPage;
 
 @Slf4j
@@ -10,11 +11,18 @@ public class LoginTest extends AbstractTest {
 
     @Test
     public void shouldSuccessfullyLogin() {
-        String email = ConfigProvider.get("credentials.email");
-        String password = ConfigProvider.get("credentials.password");
         new LoginPage(driver)
                 .openPage()
-                .attemptLogin(email, password)
+                .attemptLogin("admin", "admin", LoggedInHomePage.class)
                 .verifyIsLoaded("Slawomir", "awesome@testing.com");
+    }
+
+    @Test
+    public void shouldShowAlertOfInvalidCredentials() {
+        new LoginPage(driver)
+                .openPage()
+                .attemptLogin("wrong", "wrong", LoginPage.class)
+                .getToast()
+                .verifyErrorMessage("Invalid username/password");
     }
 }
