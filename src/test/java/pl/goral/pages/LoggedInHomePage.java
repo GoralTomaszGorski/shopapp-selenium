648571bd48241c4a33316e73e03af8ee19bdd8e6
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-04-05 12:33:11 +0200

    pages add backend url

--- src/test/java/pl/goral/pages/LoggedInHomePage.java
+++ src/test/java/pl/goral/pages/LoggedInHomePage.java
@@ -1,16 +1,22 @@
 package pl.goral.pages;
 
+import lombok.Getter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import pl.goral.pages.components.LoggedInHeader;
 
 public class LoggedInHomePage extends BasePage {
 
     private static final By EMAIL_FIELD = By.cssSelector("p.text-lg.text-gray-600");
     private static final By NAME_HEADER = By.cssSelector(".font-bold.mb-2");
 
+    @Getter
+    private final LoggedInHeader loggedInHeader;
+
     public LoggedInHomePage(WebDriver driver) {
         super(driver);
+        this.loggedInHeader = new LoggedInHeader(driver);
     }
 
     public void verifyIsLoaded(String firstName, String email) {
