ebc921f593a1d53e625183e086aa93bc8a4f5747
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-04-14 21:52:47 +0200

    suit class

---
+++ src/test/java/pl/goral/AwesomeTestSuite.java
@@ -0,0 +1,13 @@
+package pl.goral;
+
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+
+@Suite
+@SelectPackages("pl.goral.tests")
+
+public class AwesomeTestSuite {
+
+}
