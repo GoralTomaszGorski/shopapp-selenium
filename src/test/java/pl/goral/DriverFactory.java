41b9a5c33174a3a168456d1826c6b32190279945
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-04-30 20:44:37 +0200

    webdrivermanager dependency and driver menager

---
+++ src/test/java/pl/goral/DriverFactory.java
@@ -0,0 +1,31 @@
+package pl.goral;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.IOException;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver() throws IOException {
+        String name = PropertiesLoader.loadProperty("browser.name").toLowerCase();
+
+        switch (name) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+        }
+    }
+}
