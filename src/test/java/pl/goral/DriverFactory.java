c824ab5ce8d77ba645f539962b7a396c6bb542eb
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-05-13 19:44:39 +0200

    add browser prop

--- src/test/java/pl/goral/DriverFactory.java
+++ src/test/java/pl/goral/DriverFactory.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import pl.goral.config.ConfigProvider;
 
 import java.io.IOException;
 
 public class DriverFactory {
 
     public static WebDriver getDriver() throws IOException {
-        String name = PropertiesLoader.loadProperty("browser.name").toLowerCase();
+        String name = ConfigProvider.get("browser.name").toLowerCase();
 
         switch (name) {
             case "firefox":
