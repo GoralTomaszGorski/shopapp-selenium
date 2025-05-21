0e2cbab39df8c09e056c9c53c92eacd9152f9959
Tomasz Gorski <tomasz.gorski88@gmail.com>
2025-05-21 19:48:13 +0200

    screenshot extensions

--- src/test/java/pl/goral/extensions/ScreenshotTakerExtension.java
+++ src/test/java/pl/goral/extensions/ScreenshotTakerExtension.java
@@ -1,13 +1,16 @@
 package pl.goral.extensions;
 
+import io.qameta.allure.Allure;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,6 +19,7 @@ import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Order(1)
 @Slf4j
 public class ScreenshotTakerExtension implements AfterTestExecutionCallback {
 
@@ -26,33 +30,50 @@ public class ScreenshotTakerExtension implements AfterTestExecutionCallback {
 
     @Override
     public void afterTestExecution(ExtensionContext context) {
-        if (context.getExecutionException().isPresent()) {
-            log.info("Detected test failure, attempting to create a screenshot");
+        context.getExecutionException().ifPresent(throwable -> {
+            logError(context, throwable);
+
             try {
                 Path screenshotDir = Paths.get(SCREENSHOT_DIR);
                 ensureTheScreenshotDirectoryExists(screenshotDir);
+
                 String filename = getFilename(context);
                 Path destinationPath = screenshotDir.resolve(filename);
-                takeScreenshotAndSaveToFile(destinationPath);
+
+                byte[] screenshotBytes = takeScreenshotAndSaveToFile(destinationPath);
+                attachScreenshotToAllureReport(screenshotBytes, filename);
             } catch (IOException e) {
                 log.error("Failed to save screenshot", e);
             } catch (Exception e) {
                 log.error("Unexpected error during screenshot capture", e);
             }
-        }
+        });
     }
 
-    private void takeScreenshotAndSaveToFile(Path destinationPath) throws IOException {
+    private void logError(ExtensionContext context, Throwable throwable) {
+        log.error("Test {}#{} failed with an exception:",
+                context.getRequiredTestClass().getSimpleName(),
+                context.getRequiredTestMethod().getName(),
+                throwable);
+    }
+
+    private byte[] takeScreenshotAndSaveToFile(Path destinationPath) throws IOException {
         File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        byte[] screenshotBytes = Files.readAllBytes(screenshotFile.toPath());
         Files.copy(screenshotFile.toPath(), destinationPath);
         log.info("Screenshot saved to: {}", destinationPath.toAbsolutePath());
+        return screenshotBytes;
+    }
+
+    private void attachScreenshotToAllureReport(byte[] screenshotBytes, String name) {
+        Allure.addAttachment("Screenshot: " + name, "image/png", new ByteArrayInputStream(screenshotBytes), ".png");
     }
 
     private String getFilename(ExtensionContext context) {
         String className = context.getRequiredTestClass().getSimpleName();
         String methodName = context.getRequiredTestMethod().getName();
         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return String.format("%s_%s_%s.jpg", className, methodName, timestamp);
+        return String.format("%s_%s_%s.png", className, methodName, timestamp);
     }
 
     private void ensureTheScreenshotDirectoryExists(Path screenshotDir) throws IOException {
@@ -60,5 +81,4 @@ public class ScreenshotTakerExtension implements AfterTestExecutionCallback {
             Files.createDirectories(screenshotDir);
         }
     }
-
 }
